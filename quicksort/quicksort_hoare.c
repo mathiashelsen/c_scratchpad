5869b84f36149f2a07abd3f188b8ac759597b084
Mathias Helsen <helsen.mathias@gmail.com>
2021-08-29 13:52:16 +0200

    adding Hoare version of Quicksort

---
+++ quicksort/quicksort_hoare.c
@@ -0,0 +1,11 @@
+#include "quicksort_hoare.h"
+
+void quicksort_h(unsigned long **A, int lo, int hi)
+{
+
+}
+
+int  partition_h(unsigned long **A, int lo, int hi)
+{
+    return 0;
+}
---
+++ quicksort/quicksort_hoare.h
@@ -0,0 +1,7 @@
+#ifndef _QUICKSORT_HOARE_H
+#define _QUICKSORT_HOARE_H
+
+int  partition_h(unsigned long **A, int lo, int hi);
+void quicksort_h(unsigned long **A, int lo, int hi);
+
+#endif
