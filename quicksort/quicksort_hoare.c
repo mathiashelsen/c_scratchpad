e5d5b067dc24f07f1f66fb6846284c032f0a3da2
Mathias Helsen <helsen.mathias@gmail.com>
2021-08-29 15:26:23 +0200

    second quicksort also works

--- quicksort/quicksort_hoare.c
+++ quicksort/quicksort_hoare.c
@@ -1,11 +1,64 @@
 #include "quicksort_hoare.h"
 
-void quicksort_h(unsigned long **A, int lo, int hi)
+void quicksort_h(unsigned long **A, int lo, int hi, bool debug)
 {
+    int p;
+
+    if(debug)
+    {
+        printf("lo = %d, hi = %d\n", lo, hi);
+        printf("A = [");
+        for(int i = lo; i <= hi; i++)
+        {
+            printf("%lu ", (*A)[i]);
+        };
+        printf("]\n");
+    }
+    if(lo < hi && lo >= 0 && hi >= 0)
+    {
+        p = partition_h(A, lo, hi, debug);
+
+        quicksort_h(A, lo,  p, debug);
+        quicksort_h(A, p+1, hi , debug);
+    }
 
 }
 
-int  partition_h(unsigned long **A, int lo, int hi)
+int  partition_h(unsigned long **A, int lo, int hi, bool debug)
 {
-    return 0;
+    bool enableLoop = true;
+    unsigned long pivotValue = (*A)[lo];
+    unsigned long tmp;
+    int i = lo-1;
+    int j = hi+1;
+
+    if(debug)
+        printf("pivotValue = %lu\n", pivotValue);
+
+    while(enableLoop)
+    {
+        do{
+            i++;
+        }
+        while((*A)[i] < pivotValue);
+
+        do{
+            j--;
+        }
+        while((*A)[j] > pivotValue);
+
+        if(i >= j) {
+            enableLoop = false;
+        }else{
+            if(debug)
+                printf("i = %d, j = %d\n", i, j);
+
+            tmp = (*A)[i];
+            (*A)[i] = (*A)[j];
+            (*A)[j] = tmp;
+        }
+    }
+
+    return j;
+
 }
--- quicksort/quicksort_hoare.h
+++ quicksort/quicksort_hoare.h
@@ -1,7 +1,12 @@
 #ifndef _QUICKSORT_HOARE_H
 #define _QUICKSORT_HOARE_H
 
-int  partition_h(unsigned long **A, int lo, int hi);
-void quicksort_h(unsigned long **A, int lo, int hi);
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void quicksort_h(unsigned long **A, int lo, int hi, bool debug);
+int  partition_h(unsigned long **A, int lo, int hi, bool debug);
 
 #endif
